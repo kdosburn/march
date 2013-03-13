ea960c02f4a653c907f409d5ec720d2abc1357e8
Kendra Osburn <kendra@Kendras-MacBook-Pro.local>
2013-03-12 19:21:12 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarchApp::Application.config.secret_token = '8af66b1456fbc7c2189839b3d2bd215326bc7291405ecce5dcdb10b5d6e60d2f0e1f899c543d16daac06a77dbf4ad61bf503799fece2e9f8f2b5a20fb7ff505f'
